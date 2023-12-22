d36515e855c102f44ffc2816741bfb9d857431dd
smellslikeml <terry.j.rodriguez@gmail.com>
2023-12-22 06:33:12 +0000

    fixed reqs, tested lazy download

--- localmentor/utils.py
+++ localmentor/utils.py
@@ -3,18 +3,22 @@ import requests
 
 def download_mentor():
     bin_dir = os.path.join(os.path.dirname(__file__), 'bin')
-    ffmp_path = os.path.join(bin_dir, 'mentor')
+    mentor_path = os.path.join(bin_dir, 'mentor')
 
     # Create the 'bin/' directory if it does not exist
     if not os.path.exists(bin_dir):
         os.makedirs(bin_dir, exist_ok=True)
 
-    # Download ffmp file if it does not exist
-    if not os.path.exists(ffmp_path):
+    # Download mentor file if it does not exist
+    if not os.path.exists(mentor_path):
         print("Downloading mentor...")
         model_url = "https://remyx.ai/assets/localmentor/0.0.1/mentor"
         response = requests.get(model_url, stream=True)
-        with open(ffmp_path, 'wb') as f:
+        with open(mentor_path, 'wb') as f:
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
+
+        # Make the file executable
+        os.chmod(mentor_path, 0o755)
+
         print("Download complete.")
